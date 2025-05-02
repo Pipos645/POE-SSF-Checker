98b3e0ff09c5d4d8bf015abfe25be4a3f97b9039
Pipos645 <125818219+Pipos645@users.noreply.github.com>
2025-05-02 19:17:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,36 @@
+import pandas as pd
 import streamlit as st
+import requests
+from bs4 import BeautifulSoup
 
-st.title('🎈 App Name')
+st.title("POE SSF CHECKER")
 
-st.write('Hello world!')
+unique_list = pd.read_csv("poe_uniques_3.25.csv")
+
+user_link = st.text_input("Paste your pobb.in link below")
+
+if user_link != "":
+    r = requests.get(user_link)
+    soup = BeautifulSoup(r.text, "lxml")
+    inventory = soup.select("div.inventory > img")
+    inventory_list = []
+
+    for items in inventory:
+        inventory_list.append(items["alt"])
+
+    match = unique_list[unique_list["Name"].isin(inventory_list)]
+    match["Wiki link"] = "https://www.poewiki.net/wiki/" + match["Name"]
+  
+    st.markdown(f":orange[{len(match)} unique items detected]")
+    st.markdown("**Table containing their names, rarity tier and link to wiki :**")
+  
+    st.dataframe(
+        match,
+        column_config={
+            "Wiki link": st.column_config.LinkColumn(
+                display_text="wiki"
+            )
+        },
+        hide_index=True
+    )
+    st.markdown(":gray[Made by Philippe Brand 2025, this website is not affiliated with GGG by any means.]")
